e5b6993a1d06c0a65e359c08e04c6fb8cfc864f0
Victor Yeo <you@example.com>
2020-05-26 19:02:31 +0800

    add feature rspec

---
+++ spec/features/add_video_url.rb
@@ -0,0 +1,26 @@
+require "rails_helper"
+
+RSpec.feature 'The features', :type => :feature do
+  given!(:user) {User.create!(email: "2@2.com", password: "123456")}
+
+  scenario 'login to page' do
+    pp user.id
+    visit new_user_session_path
+    find('div.useremail').fill_in 'user_email', :with => 'email'
+    find('div.userpd').fill_in 'user_password', with: ''
+    click_on 'Log in'
+  end
+  scenario 'they see the video on the page' do
+    pp user.id
+    visit new_user_session_path
+    find('div.useremail').fill_in 'user_email', :with => 'email'
+    find('div.userpd').fill_in 'user_password', with: ''
+    click_on 'Log in'
+    page.set_rack_session(current_user: user)
+    visit new_video_path
+    fill_in 'video_video_url', :with => 'https://www.youtube.com'
+    click_button 'Share Video'
+
+    expect(page).to have_text 'Back to videos'
+  end
+end
