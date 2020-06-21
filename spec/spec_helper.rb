904428a3c9c9912cfe46b9b664d79ef7d491a45f
Victor Yeo <you@example.com>
2020-06-21 14:14:08 +0800

    remove Devise::TestHelpers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -95,6 +95,4 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
-  #config.include Devise::TestHelpers, type: :controller
-  #config.extend ControllerMacros, type: :controller
 end
